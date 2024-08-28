17e3d2b48b6c79705a36d3be1e372dd3409e5b54
ydourado <yago.dourado@mercadolivre.com>
2024-08-28 12:49:17 -0300

    chore: add Google Analytics script to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Olavo disse hoje",
-  description: "Seleção de melhores frases do professor Olavo de Carvalho, atualizadas diariamente.",
+  description:
+    "Seleção de melhores frases do professor Olavo de Carvalho, atualizadas diariamente.",
 };
 
 export default function RootLayout({
@@ -17,6 +19,19 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7DWNTK36BC"
+        strategy="afterInteractive"
+      />
+
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7DWNTK36BC');
+        `}
+      </Script>
     </html>
   );
 }
